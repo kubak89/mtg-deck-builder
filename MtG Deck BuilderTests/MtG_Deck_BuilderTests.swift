1c4fafd1f3d1c36762f3d0342d9fd3c98afa197b
Jakub Komorowski <jakub.komorowski@codequest.com>
2019-06-07 09:52:33 +0200

    Initial Commit

---
+++ MtG Deck BuilderTests/MtG_Deck_BuilderTests.swift
@@ -0,0 +1,34 @@
+//
+//  MtG_Deck_BuilderTests.swift
+//  MtG Deck BuilderTests
+//
+//  Created by Jakub Komorowski on 07/06/2019.
+//  Copyright © 2019 Jakub Komorowski. All rights reserved.
+//
+
+import XCTest
+@testable import MtG_Deck_Builder
+
+class MtG_Deck_BuilderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
